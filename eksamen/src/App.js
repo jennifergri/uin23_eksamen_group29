c4c65588afe62edebaa0108c4d828184ada3e892
stinevaagnes <stinevaagnes@gmail.com>
2023-04-24 13:16:04 +0200

    Satt opp komponenter i "components"-mappa + routing
    Alle var med på dette <3

--- eksamen/src/App.js
+++ eksamen/src/App.js
@@ -1,9 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import './css/main.css';
+import Dashboard from './components/Dashboard';
+import GameShop from './components/GameShop';
+import MyGames from './components/MyGames';
+import MyFavourites from './components/MyFavourites';
 
 function App() {
   return (
     <>
+    <Routes>
+      <Route path="/dashboard" element={<Dashboard />}/>
+      <Route path="/gameshop" element={<GameShop />}/>
+      <Route path="/mygames" element={<MyGames />}/>
+      <Route path="/favourites" element={<MyFavourites />}/>
+    </Routes>
     </>
   );
 }
