cefb93ab526e8a9a2a1625e2035473805d2c4273
Mariannemyrvold <marmyr6@hotmail.com>
2023-04-28 12:01:15 +0200

    La inn layout og start på de andre components

--- eksamen/src/components/Dashboard.js
+++ eksamen/src/components/Dashboard.js
@@ -1,5 +1,26 @@
 export default function Dashboard(){
     return(
-        <h1>GameShop</h1>
+    <div>
+        <header> 
+            <h1>Girly Girls Games</h1>
+        </header>
+        <main>
+            <h2>GAMESHOP</h2>
+        
+            <section>
+                <article></article>
+            </section>
+
+            <section>
+                <h2>MY GAMES-LIBRARY</h2>
+            </section>
+
+            <section>
+                <h2>MY FAVOURITES</h2>
+            </section>
+            
+        </main>
+    </div>
+
     )
 }
--- eksamen/src/components/MyGames.js
+++ eksamen/src/components/MyGames.js
@@ -1,3 +1,5 @@
 export default function MyGames(){
-    return
+    return (
+        <></>
+    )
 }
